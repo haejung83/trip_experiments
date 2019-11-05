edde2675a7f1a7a1b1c72013dfb321cd0f056ebc
김해중 <gnu@tripstore.kr>
2019-11-06 08:57:10 +0900

    데이터 바인딩 기능 활성화 및 데이터 바인딩 기반 클래스 추가
    데이터 바인딩 기반 클래스는 뷰에 해당하는 엑티비티/프래그먼트 두가지만 정의
    
    다이얼로그등은 필요할 대 추가 예정

---
+++ app/src/main/java/kr/tripstore/proto/presentation/base/DataBindingFragment.kt
@@ -0,0 +1,31 @@
+package kr.tripstore.proto.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class DataBindingFragment<T : ViewDataBinding> : Fragment() {
+
+    protected lateinit var viewDataBinding: T
+        private set
+
+    abstract val layoutResId: Int
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) =
+        DataBindingUtil.inflate<T>(inflater, layoutResId, container, false).apply {
+            viewDataBinding = this
+        }.root
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
+    }
+
+}
