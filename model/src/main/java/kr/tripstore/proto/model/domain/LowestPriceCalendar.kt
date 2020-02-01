160dc5077c8c7cfca83466c16e119a4ccc6160f0
Hae-Jung Kim <haejung83@gmail.com>
2020-02-01 12:17:42 +0900

    캘린더 DTO를 일반 클래스에서 데이터클래스로 변경

--- model/src/main/java/kr/tripstore/proto/model/domain/LowestPriceCalendar.kt
+++ model/src/main/java/kr/tripstore/proto/model/domain/LowestPriceCalendar.kt
@@ -1,18 +1,38 @@
 package kr.tripstore.proto.model.domain
 
-class LowestPriceCalendar(
+data class LowestPriceCalendar(
     val placeId: Int,
     val cityId: Array<Int>,
     val months: List<LowestPriceMonth>
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
 
-class LowestPriceMonth(
+        other as LowestPriceCalendar
+
+        if (placeId != other.placeId) return false
+        if (!cityId.contentEquals(other.cityId)) return false
+        if (months != other.months) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = placeId
+        result = 31 * result + cityId.contentHashCode()
+        result = 31 * result + months.hashCode()
+        return result
+    }
+}
+
+data class LowestPriceMonth(
     val month: Int,
     val highestTemperatures: String,
     val days: List<LowestPriceDay>
 )
 
-class LowestPriceDay(
+data class LowestPriceDay(
     val day: Int,
     val price: Int,
     val isHoliday: Boolean
