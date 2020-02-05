b8834793d5c6f9ccc2aefc477616b45f2017e76b
Hae-Jung Kim <haejung83@gmail.com>
2020-02-05 23:02:16 +0900

    빠진 캘린더 화면을 위한 데이터 아이템 추가

---
+++ app/src/main/java/kr/tripstore/proto/presentation/calendar/CalendarItem.kt
@@ -0,0 +1,59 @@
+package kr.tripstore.proto.presentation.calendar
+
+abstract class CalendarItem {
+    abstract val calendarItemType: CalendarItemType
+}
+
+data class CalendarTitleItem(
+    val title: String
+) : CalendarItem() {
+    override val calendarItemType: CalendarItemType
+        get() = CalendarItemType.CALENDAR_TITLE
+}
+
+data class CalendarSpaceItem(
+    val height: Int
+) : CalendarItem() {
+    override val calendarItemType: CalendarItemType
+        get() = CalendarItemType.CALENDAR_SPACE
+}
+
+data class CalendarMonthTitleItem(
+    val monthTitle: String,
+    val highestTemperature: String
+) : CalendarItem() {
+    override val calendarItemType: CalendarItemType
+        get() = CalendarItemType.CALENDAR_MONTH_TITLE
+}
+
+data class CalendarDayOfWeekItem(
+    val dayOfWeek: String,
+    val isHoliday: Boolean
+) : CalendarItem() {
+    override val calendarItemType: CalendarItemType
+        get() = CalendarItemType.CALENDAR_DAY_OF_WEEK_CELL
+}
+
+enum class PriceGrade {
+    EXPENSIVE,
+    REASONABLE,
+    CHEAP,
+    NONE
+}
+
+data class CalendarDayCellItem(
+    val day: Int,
+    val lowestPrice: Int,
+    val gradeOfPrice: PriceGrade,
+    val isHoliday: Boolean
+) : CalendarItem() {
+    override val calendarItemType: CalendarItemType
+        get() = CalendarItemType.CALENDAR_DAY_CELL
+}
+
+data class CalendarEmptyCell(
+    val emptyCellCount: Int
+) : CalendarItem() {
+    override val calendarItemType: CalendarItemType
+        get() = CalendarItemType.CALENDAR_EMPTY_CELL
+}
