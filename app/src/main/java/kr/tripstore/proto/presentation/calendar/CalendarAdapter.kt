284a8b2eb4d11678dd7e27f7886a5da1c60ca852
Hae-Jung Kim <haejung83@gmail.com>
2020-02-06 21:50:28 +0900

    캘린더 화면표시를 위한 리스트 아답터 구현

--- app/src/main/java/kr/tripstore/proto/presentation/calendar/CalendarAdapter.kt
+++ app/src/main/java/kr/tripstore/proto/presentation/calendar/CalendarAdapter.kt
@@ -1,11 +1,91 @@
 package kr.tripstore.proto.presentation.calendar
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import kr.tripstore.proto.databinding.*
 
-class CalendarAdapter {
+class CalendarAdapter(
+    private val calendarItemViewClickListener: CalendarItemViewClickListener
+) : ListAdapter<CalendarItem, CalendarItemViewHolder>(CalendarItemDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalendarItemViewHolder =
+        when (viewType) {
+            CalendarItemType.CALENDAR_DAY_CELL.viewType ->
+                CalendarDayCellItemViewHolder(
+                    ItemCalendarDayCellBinding.inflate(
+                        LayoutInflater.from(parent.context), parent, false
+                    )
+                )
+            CalendarItemType.CALENDAR_EMPTY_CELL.viewType ->
+                CalendarEmptyCellItemViewHolder(
+                    ItemCalendarEmptyBinding.inflate(
+                        LayoutInflater.from(parent.context), parent, false
+                    )
+                )
+            CalendarItemType.CALENDAR_DAY_OF_WEEK_CELL.viewType ->
+                CalendarDayCellItemViewHolder(
+                    ItemCalendarDayCellBinding.inflate(
+                        LayoutInflater.from(parent.context), parent, false
+                    )
+                )
+            CalendarItemType.CALENDAR_MONTH_TITLE.viewType ->
+                CalendarMonthTitleItemViewHolder(
+                    ItemCalendarMonthTitleBinding.inflate(
+                        LayoutInflater.from(parent.context), parent, false
+                    )
+                )
+            CalendarItemType.CALENDAR_SPACE.viewType ->
+                CalendarSpaceItemViewHolder(
+                    ItemCalendarSpaceBinding.inflate(
+                        LayoutInflater.from(parent.context), parent, false
+                    )
+                )
+            CalendarItemType.CALENDAR_TITLE.viewType ->
+                CalendarTitleItemViewHolder(
+                    ItemCalendarTitleBinding.inflate(
+                        LayoutInflater.from(parent.context), parent, false
+                    )
+                )
+            else -> throw Throwable("Couldn't inflate with a given CalendarItemType")
+        }
+
+    override fun onBindViewHolder(holder: CalendarItemViewHolder, position: Int) =
+        holder.bind(getItem(position), calendarItemViewClickListener)
+
+    override fun getItemViewType(position: Int): Int = getItem(position).calendarItemType.viewType
 
 }
 
 interface CalendarItemViewClickListener {
     fun onClick(view: View, calendarItem: CalendarItem)
+}
+
+class CalendarItemDiffCallback : DiffUtil.ItemCallback<CalendarItem>() {
+
+    override fun areItemsTheSame(oldItem: CalendarItem, newItem: CalendarItem): Boolean =
+        oldItem.hashCode() == newItem.hashCode()
+
+    override fun areContentsTheSame(oldItem: CalendarItem, newItem: CalendarItem): Boolean =
+        if (oldItem.calendarItemType == newItem.calendarItemType) {
+            when (oldItem.calendarItemType) {
+                CalendarItemType.CALENDAR_DAY_CELL ->
+                    (oldItem as CalendarDayCellItem) == (newItem as CalendarDayCellItem)
+                CalendarItemType.CALENDAR_EMPTY_CELL ->
+                    (oldItem as CalendarEmptyCellItem) == (newItem as CalendarEmptyCellItem)
+                CalendarItemType.CALENDAR_DAY_OF_WEEK_CELL ->
+                    (oldItem as CalendarDayOfWeekItem) == (newItem as CalendarDayOfWeekItem)
+                CalendarItemType.CALENDAR_MONTH_TITLE ->
+                    (oldItem as CalendarMonthTitleItem) == (newItem as CalendarMonthTitleItem)
+                CalendarItemType.CALENDAR_SPACE ->
+                    (oldItem as CalendarSpaceItem) == (newItem as CalendarSpaceItem)
+                CalendarItemType.CALENDAR_TITLE ->
+                    (oldItem as CalendarTitleItem) == (newItem as CalendarTitleItem)
+            }
+        } else {
+            false
+        }
+
 }
