2daeeda48af1ff1d686fb6dba9e08af8dd4370fe
김해중 <gnu@tripstore.kr>
2020-02-10 15:16:06 +0900

    Skeleton Loading의 레이아웃 및 바인딩아답터 추가

---
+++ app/src/main/java/kr/tripstore/proto/presentation/skeleton/SkeletonBindings.kt
@@ -0,0 +1,59 @@
+package kr.tripstore.proto.presentation.skeleton
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.BindingAdapter
+import io.supercharge.shimmerlayout.ShimmerLayout
+import kr.tripstore.proto.R
+import kotlin.math.floor
+
+object SkeletonBindings {
+
+    @BindingAdapter("shimmer_is_show")
+    @JvmStatic
+    fun setShimmerShow(shimmerLayout: ShimmerLayout, isShow: Boolean?) {
+        isShow?.let {
+            shimmerLayout.visibility = if (isShow) View.VISIBLE else View.GONE
+            if (isShow) shimmerLayout.startShimmerAnimation()
+            else shimmerLayout.stopShimmerAnimation()
+        }
+    }
+
+    @BindingAdapter("shimmer_item_layout")
+    @JvmStatic
+    fun setShimmerItemLayout(shimmerLayout: ShimmerLayout, @LayoutRes skeletonItemLayoutId: Int?) {
+        skeletonItemLayoutId?.let { itemLayoutId ->
+            val measuredHeight =
+                getMeasuredHeightOfShimmerItem(shimmerLayout.context, itemLayoutId)
+
+            // Calculate number of child using both the height of device and the height of child view
+            shimmerLayout.findViewById<ViewGroup>(R.id.linearlayout_skeleton_container).run {
+                val count = getCountOfShimmerItem(shimmerLayout.context, measuredHeight)
+                repeat(count) {
+                    // Add child view by calculated counts
+                    addView(LayoutInflater.from(context).inflate(itemLayoutId, null))
+                }
+            }
+        }
+    }
+
+    // FIXME: Inflate a view for measuring, It's just cost, not graceful.
+    private fun getMeasuredHeightOfShimmerItem(context: Context, @LayoutRes layoutId: Int) =
+        LayoutInflater.from(context).inflate(layoutId, null).run {
+            measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+            measuredHeight
+        }
+
+    private fun getCountOfShimmerItem(context: Context, heightOfShimmerItem: Int): Int =
+        if (heightOfShimmerItem > 0) {
+            floor(
+                context.resources.displayMetrics.heightPixels.toFloat() / heightOfShimmerItem.toFloat()
+            ).toInt()
+        } else {
+            0
+        }
+
+}
