5bb7cc1e6229c47615845296a3bd4bb8ecae9d26
Hae-Jung Kim <haejung83@gmail.com>
2020-02-26 22:52:47 +0900

    바텀앱바 테스트 중

--- app/src/main/java/kr/tripstore/proto/presentation/trip/TripActivity.kt
+++ app/src/main/java/kr/tripstore/proto/presentation/trip/TripActivity.kt
@@ -2,10 +2,12 @@ package kr.tripstore.proto.presentation.trip
 
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_trip.*
 import kr.tripstore.proto.R
+import timber.log.Timber
 
 class TripActivity : DaggerAppCompatActivity() {
 
@@ -17,8 +19,18 @@ class TripActivity : DaggerAppCompatActivity() {
     }
 
     private fun setupBottomNavigationWithNavController() {
-        val navController = findNavController(R.id.navhostfragment_trip)
-        bottomnavigationview_trip.setupWithNavController(navController)
+//        val navController = findNavController(R.id.navhostfragment_trip)
+//        bottomappbar_trip.setupWithNavController(navController)
+
+        bottomappbar_trip.apply {
+            setOnMenuItemClickListener {
+                Timber.d("setOnMenuItemClickListener: $it")
+                true
+            }
+            setNavigationOnClickListener {
+                Timber.d("setNavigationOnClickListener: $it")
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
